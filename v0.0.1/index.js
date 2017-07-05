1773d20e1eab0331f0d88a6f0108278b456368e8
Benjamin Saphier <b.saphier@gmail.com>
2017-07-04 21:07:25 -0400

    removed unused and old files

--- v0.0.1/index.js
+++
@@ -1,7 +0,0 @@
-import * as audioActionCreators from './audio-actions/action-creators';
-import audioContextProvider from './audioContextProvider';
-
-module.exports = {
-  audioActionCreators,
-  audioContextProvider
-};
