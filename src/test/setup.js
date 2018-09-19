b47387c7d3c49a012fe243695ecfc84370b9a5c6
Benjamin Saphier <b.saphier@gmail.com>
2018-09-19 17:05:31 -0400

    removed comment

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,7 +1,2 @@
 import { RenderingAudioContext } from 'web-audio-engine';
 window.AudioContext = RenderingAudioContext;
-
-// export default (function() {
-//   window.AudioContext = RenderingAudioContext;
-//   global.AudioContext = RenderingAudioContext;
-// })();
